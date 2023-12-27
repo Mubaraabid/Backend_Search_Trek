dbc485705ccd2dbf4e61c48ee30a0cbdf7d2d85a
mubara abid <mubaraabid9@gmail.com>
2023-12-27 15:18:52 -0800

    project setup

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+    const uri = process.env.MONGODB_CONNECTION_URI;
+    mongoose.connect(uri,{
+        autoCreate: true,
+        autoIndex: true
+    }).then((res)=>{
+        console.log("DB Connected Successfully")
+    }).catch((err)=>{
+        console.log("Alas! DB Connection Error");
+    })
+}
+
+export default connectDB;
